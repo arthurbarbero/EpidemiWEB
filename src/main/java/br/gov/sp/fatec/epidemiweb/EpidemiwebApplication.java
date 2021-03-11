09e45832d1b787b00f4ec4d4b924f08c8b24ea09
Arthur Barbero <arthur-barbero@hotmail.com>
2021-03-11 20:32:10 -0300

    Initial commit

---
+++ src/main/java/br/gov/sp/fatec/epidemiweb/EpidemiwebApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.epidemiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpidemiwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/epidemiweb/EpidemiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.epidemiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
